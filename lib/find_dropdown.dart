f279aa7f461162cd5250c7e2f1182a8240a21092
David Araujo <davidsdearaujo@gmail.com>
2019-09-25 21:25:35 -0300

    [#1] Added parameter "validate"

--- lib/find_dropdown.dart
+++ lib/find_dropdown.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 
 import 'find_dropdown_bloc.dart';
 
+typedef Future<List<T>> FindDropdownFindType<T>(String text);
+typedef void FindDropdownChangedType<T>(T selectedItem);
 typedef Widget FindDropdownBuilderType<T>(BuildContext context, T selectedText);
+typedef String FindDropdownValidationType<T>(T selectedText);
 typedef Widget FindDropdownItemBuilderType<T>(
     BuildContext context, T item, bool isSelected);
 
@@ -14,10 +17,11 @@ class FindDropdown<T> extends StatefulWidget {
   final TextStyle labelStyle;
   final List<T> items;
   final T selectedItem;
-  final Future<List<T>> Function(String text) onFind;
-  final void Function(T selectedItem) onChanged;
+  final FindDropdownFindType<T> onFind;
+  final FindDropdownChangedType<T> onChanged;
   final FindDropdownBuilderType<T> dropdownBuilder;
   final FindDropdownItemBuilderType<T> dropdownItemBuilder;
+  final FindDropdownValidationType<T> validate;
 
   const FindDropdown({
     Key key,
@@ -29,6 +33,7 @@ class FindDropdown<T> extends StatefulWidget {
     this.onFind,
     this.dropdownBuilder,
     this.dropdownItemBuilder,
+    this.validate,
   })  : assert(onChanged != null),
         super(key: key);
   @override
@@ -41,7 +46,10 @@ class _FindDropdownState<T> extends State<FindDropdown<T>> {
   @override
   void initState() {
     super.initState();
-    bloc = FindDropdownBloc<T>(seedValue: widget.selectedItem);
+    bloc = FindDropdownBloc<T>(
+      seedValue: widget.selectedItem,
+      validate: widget.validate,
+    );
   }
 
   @override
@@ -62,53 +70,77 @@ class _FindDropdownState<T> extends State<FindDropdown<T>> {
             style: widget.labelStyle ?? Theme.of(context).textTheme.subhead,
           ),
         if (widget.label != null) SizedBox(height: 5),
-        StreamBuilder<T>(
-          stream: bloc.selected$,
-          builder: (context, snapshot) {
-            return GestureDetector(
-              onTap: () {
-                SelectDialog.showModal(
-                  context,
-                  items: widget.items,
-                  label: widget.label,
-                  onFind: widget.onFind,
-                  itemBuilder: widget.dropdownItemBuilder,
-                  onChange: (item) {
-                    bloc.selected$.add(item);
-                    widget.onChanged(item);
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            StreamBuilder<T>(
+              stream: bloc.selected$,
+              builder: (context, snapshot) {
+                return GestureDetector(
+                  onTap: () {
+                    SelectDialog.showModal(
+                      context,
+                      items: widget.items,
+                      label: widget.label,
+                      onFind: widget.onFind,
+                      itemBuilder: widget.dropdownItemBuilder,
+                      onChange: (item) {
+                        bloc.selected$.add(item);
+                        widget.onChanged(item);
+                      },
+                      selectedValue: snapshot.data,
+                    );
                   },
-                  selectedValue: snapshot.data,
+                  child: (widget.dropdownBuilder != null)
+                      ? widget.dropdownBuilder(context, snapshot.data)
+                      : Container(
+                          padding: EdgeInsets.fromLTRB(15, 5, 5, 5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              width: 1,
+                              color: Theme.of(context).dividerColor,
+                            ),
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(snapshot.data?.toString() ?? ""),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: Icon(
+                                  Icons.arrow_drop_down,
+                                  size: 25,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                 );
               },
-              child: (widget.dropdownBuilder != null)
-                  ? widget.dropdownBuilder(context, snapshot.data)
-                  : Container(
-                      padding: EdgeInsets.fromLTRB(15, 5, 5, 5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          width: 1,
-                          color: Theme.of(context).dividerColor,
-                        ),
-                        borderRadius: BorderRadius.circular(4.0),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(snapshot.data?.toString() ?? ""),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Icon(
-                              Icons.arrow_drop_down,
-                              size: 25,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ],
-                      ),
+            ),
+            StreamBuilder<String>(
+              stream: bloc.validateMessageOut,
+              builder: (context, snapshot) {
+                return ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: 15),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Text(
+                      snapshot.data ?? "",
+                      style: Theme.of(context).textTheme.body1.copyWith(
+                          color: snapshot.hasData
+                              ? Theme.of(context).errorColor
+                              : Colors.transparent),
                     ),
-            );
-          },
+                  ),
+                );
+              },
+            )
+          ],
         ),
       ],
     );
